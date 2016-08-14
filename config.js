11ce582ff0a8c22f296a5b9107209506a23a2fb8
Ethan Brooks <EB5473@gmail.com>
2016-08-14 05:54:36 -0400

    Added comments to configuration file

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 var config = {}
 
+// Configures the bound host for the Gopher server
+// *CURRENTLY NOT FUNCTIONING*
 config.host = "localhost";
+
+// Configures the bound port for the Gopher server
 config.port = 70;
+
+// The file to load as the "Message Of The Day", a text file.
 config.motd = "motd.txt";
+
+// The root document, from which all other static content is derived.
 config.root = "doc";
 
 function getConfig(opt, def)
